48d41f81e2afdd072735f0e89bdb34824f1fcc96
Patrick Frank <patr.frank@gmx.de>
2020-03-12 18:38:44 +0100

    Traverse games and extract fen for all positions

--- pgn2bb.py
+++ pgn2bb.py
@@ -1,34 +1,49 @@
+#!/usr/bin/env python3
+
 import chess
 import chess.pgn
 
-def pgn2bb(file):
+def pgn2pos(file, ptype='bb'):
 
-	gamelist = []
+	game_list = []
 
-	with open(file,'r') as f:
+	with open(file, 'r') as f:
+
 		while True:
 			game = chess.pgn.read_game(f)
 
 			if game is None:
 				break  # end of file
 			else:
-				gamelist.append(game)
-	
-	
-	return gamelist
+				temp_game = gamepos(game=game, ptype=ptype)
+				game_list.append(temp_game)
+
+	return game_list
+
+def gamepos(game, ptype='bb'):
+
+	board = chess.Board()
+	game_fen = []
+
+	for move in game.mainline_moves():
+		board.push(move)
+
+		if ptype == 'fen':
+			game_fen.append(board.fen())
+		else:
+			raise ValueError("This ptype is not implemented")
+
+	return game_fen
 
 if __name__ == "__main__":
-	
-	games = pgn2bb(file = "data/test.pgn")
-	print(len(games))
-	print(games)
+
+	test_games = pgn2pos(file="data/test.pgn", ptype='fen')
+
+	for tg in test_games:
+		print(tg)
 
 #for i in range(1,6):
-#    print(board.pieces(i,0))
-#    print("\n vars",(i,0),"\n")
-#    print(board.pieces(i,1))
-#    print("\n vars",(i,1),"\n")
-
-#for move in game.mainline_moves():
-#    board.push(move)
-#    print(board.fen())
+#	print(board.pieces(i,0))
+#	print("\n vars",(i,0),"\n")
+#	print(board.pieces(i,1))
+#	print("\n vars",(i,1),"\n")
