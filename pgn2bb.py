abd405ee94327e7f926a726ebefd264c960fe4e2
Patrick Frank <patr.frank@gmx.de>
2020-03-13 20:19:46 +0100

    Write out bitboard extraction algorithm

--- pgn2bb.py
+++ pgn2bb.py
@@ -2,6 +2,8 @@
 
 import chess
 import chess.pgn
+import numpy as np
+
 
 def pgn2pos(file, ptype='bb'):
 
@@ -15,35 +17,63 @@ def pgn2pos(file, ptype='bb'):
 			if game is None:
 				break  # end of file
 			else:
-				temp_game = gamepos(game=game, ptype=ptype)
+				temp_game = game_fen(game=game, ptype=ptype)
 				game_list.append(temp_game)
 
 	return game_list
 
-def gamepos(game, ptype='bb'):
+def game_fen(game, ptype='bb'):
 
 	board = chess.Board()
-	game_fen = []
+	pos = []
 
 	for move in game.mainline_moves():
 		board.push(move)
+		s = []
+		embedding = np.array([], dtype=bool)
 
 		if ptype == 'fen':
-			game_fen.append(board.fen())
-		else:
-			raise ValueError("This ptype is not implemented")
+			pos.append(board.fen())
+		elif ptype == 'bb':
+			for i in range(1,7): # P N B R Q K / white
+				s.append(list(board.pieces(i,0)))
+				bmp = np.zeros(shape=(64,))
+				for j in range(64):
+					if j in list(board.pieces(i,1)):
+						bmp[j] = True
+				print(bmp)
+				embedding = np.concatenate((embedding,bmp))
+			for i in range(1,7): # P N B R Q K / black
+				s.append(list(board.pieces(i,0)))
+				bmp = np.zeros(shape=(64,))
+				for j in range(64):
+					if j in list(board.pieces(i,0)):
+						bmp[j] = True
+				print(bmp)
+				embedding = np.concatenate((embedding,bmp))
+			embedding = np.concatenate((embedding, [bool(board.turn)])) # white=1, black=0
+			print(s)
+			embedding = np.concatenate((embedding, [bool(board.castling_rights & chess.BB_A1)]))
+			embedding = np.concatenate((embedding, [bool(board.castling_rights & chess.BB_H1)]))
+			embedding = np.concatenate((embedding, [bool(board.castling_rights & chess.BB_A8)]))
+			embedding = np.concatenate((embedding, [bool(board.castling_rights & chess.BB_H8)]))
+			
+			print(embedding, len(embedding))
+			
 
-	return game_fen
-
-if __name__ == "__main__":
-
-	test_games = pgn2pos(file="data/test.pgn", ptype='fen')
-
-	for tg in test_games:
-		print(tg)
-
-#for i in range(1,6):
 #	print(board.pieces(i,0))
 #	print("\n vars",(i,0),"\n")
 #	print(board.pieces(i,1))
 #	print("\n vars",(i,1),"\n")
+		else:
+			raise ValueError("This ptype is not implemented")
+
+	return pos
+
+
+if __name__ == "__main__":
+
+	test_games = pgn2pos(file="data/test2.pgn", ptype='bb')
+
+	for tg in test_games:
+		print(tg)
