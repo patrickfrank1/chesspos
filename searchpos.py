bcdced82247f442a8ae3483f495d390c8149e4cf
Patrick Frank <patr.frank@gmx.de>
2020-03-19 21:08:30 +0100

    Simple woriking faiss index

--- searchpos.py
+++ searchpos.py
@@ -16,24 +16,33 @@ def faiss_load_bb(file_list):
 			for key in hf.keys():
 				if key[:8] == 'position':
 					hf_len = len(hf[key])
-					chunks = 100000
+					chunks = int(1e6)
 					vectors = np.zeros(shape=(chunks, 776), dtype=np.bool_)
 					for i in range(math.floor(hf_len/chunks)):
 						vectors[:, :773] = hf[key][i*chunks:(i+1)*chunks,:773]
-						uint_vec = np.zeros(shape=(chunks,97), dtype=np.uint8)
-						for j in range(97):
-							uint_vec[:,j] = np.packbits(vectors[:,j*8:(j+1)*8])
-						print(vectors.shape, index.d, vectors[0], uint_vec[0])
+						uint_vec = transform_bb_uint8(vectors)
 						index.add(uint_vec)
-						print(f"{index.ntotal / 1.e6} million positions stored")
+						print(f"{index.ntotal / 1.e6} million positions stored", end="\r")
+					rest_len = hf_len % chunks
+					vectors = np.zeros(shape=(rest_len, 776), dtype=np.bool_)
+					vectors[:, :773] = hf[key][math.floor(hf_len/chunks)*chunks:,:773]
+					uint_vec = transform_bb_uint8(vectors)
+					index.add(uint_vec)
+					print(f"{index.ntotal / 1.e6} million positions stored")
+	return index
 
 def transform_bb_uint8(bb_array):
-	pass
-
+	bb_len, vec_len = bb_array.shape
+	uint = np.copy(bb_array).reshape((bb_len, int(vec_len/8), 8))
+	uint = np.reshape(np.packbits(uint, axis=-1), (bb_len, int(vec_len/8)))
+	return uint
 
 if __name__ == "__main__":
 	
-	faiss_load_bb(["data/db/lichess_db_standard_rated_2013-01-bb"])
+	index = faiss_load_bb(["data/db/lichess_db_standard_rated_2013-01-bb",
+					"data/db/lichess_db_standard_rated_2013-02-bb"])
+
+	
 
 
 
