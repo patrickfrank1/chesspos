fb94728581e8dc196f59453d08d40455fa7546df
Patrick Frank <patr.frank@gmx.de>
2020-03-20 16:02:07 +0100

    Refactor index building
    Entry points:
    - load_bb: load bitboard array into binary index
    - load_h5_bb: load bitboards of selected tables of an h5 file into index
    - load_h5_array: load and process an array of h5 files

--- searchpos.py
+++ searchpos.py
@@ -4,57 +4,54 @@ import numpy as np
 import h5py
 from pgn2pos import correct_file_ending
 
-def faiss_load_bb(file_list):
-	d = 776
-	index = faiss.IndexBinaryFlat(d)
+def init_binary_index(dim, threads=4):
+	# set threads
+	faiss.omp_set_num_threads(threads)
 
-	for file in file_list:
-		fname = correct_file_ending(file, "h5")
-		with h5py.File(fname, 'r') as hf:
-			print(f"File {fname} has keys {hf.keys()}")
+	# build index
+	return faiss.IndexBinaryFlat(dim)
 
-			for key in hf.keys():
-				if key[:8] == 'position':
-					hf_len = len(hf[key])
-					chunks = int(1e6)
-					vectors = np.zeros(shape=(chunks, 776), dtype=np.bool_)
-					for i in range(math.floor(hf_len/chunks)):
-						vectors[:, :773] = hf[key][i*chunks:(i+1)*chunks,:773]
-						uint_vec = transform_bb_uint8(vectors)
-						index.add(uint_vec)
-						print(f"{index.ntotal / 1.e6} million positions stored", end="\r")
-					rest_len = hf_len % chunks
-					vectors = np.zeros(shape=(rest_len, 776), dtype=np.bool_)
-					vectors[:, :773] = hf[key][math.floor(hf_len/chunks)*chunks:,:773]
-					uint_vec = transform_bb_uint8(vectors)
-					index.add(uint_vec)
-					print(f"{index.ntotal / 1.e6} million positions stored")
-	return index
-
-def transform_bb_uint8(bb_array):
+def load_bb(bb_array, faiss_index):
 	bb_len, vec_len = bb_array.shape
 	uint = np.copy(bb_array).reshape((bb_len, int(vec_len/8), 8))
 	uint = np.reshape(np.packbits(uint, axis=-1), (bb_len, int(vec_len/8)))
-	return uint
+	faiss_index.add(uint)
+	print(f"{faiss_index.ntotal / 1.e6} million positions stored", end="\r")
+	return faiss_index
+
+def load_h5_bb(file, id_string, faiss_index, chunks=int(1e6)):
+	fname = correct_file_ending(file, "h5")
+	with h5py.File(fname, 'r') as hf:
+		print(f"File {fname} has keys {hf.keys()}")
+		for key in hf.keys():
+			if id_string in key:
+				hf_len = len(hf[key])
+				chunks = int(chunks)
+				vectors = np.zeros(shape=(chunks, 776), dtype=np.bool_)
+				for i in range(math.floor(hf_len/chunks)):
+					vectors[:, :773] = hf[key][i*chunks:(i+1)*chunks, :773]
+					faiss_index = load_bb(vectors, faiss_index)
+				rest_len = hf_len % chunks
+				vectors = np.zeros(shape=(rest_len, 776), dtype=np.bool_)
+				vectors[:, :773] = hf[key][math.floor(hf_len/chunks)*chunks:, :773]
+				faiss_index = load_bb(vectors, faiss_index)
+	return faiss_index
+
+def load_h5_array(file_list, id_string, faiss_index, chunks=int(1e6)):
+	for file in file_list:
+		faiss_index = load_h5_bb(file, id_string, faiss_index, chunks=chunks)
+
+	return faiss_index
 
 if __name__ == "__main__":
-	
-	index = faiss_load_bb(["data/db/lichess_db_standard_rated_2013-01-bb",
-					"data/db/lichess_db_standard_rated_2013-02-bb"])
 
-	
-
+	#index = faiss_load_bb(["data/db/lichess_db_standard_rated_2013-01-bb"])
+	index = init_binary_index(776)
+	index = load_h5_array(["data/db/lichess_db_standard_rated_2013-01-bb"], "position", index)
 
 
 
 
-
-
-
-# index = faiss.IndexFlatL2(d)   # build the index
-# print(index.is_trained)
-# index.add(xb)                  # add vectors to the index
-# print(index.ntotal)
 
 # k = 4                          # we want to see 4 nearest neighbors
 # D, I = index.search(xb[:5], k) # sanity check
