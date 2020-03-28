5d41a5f799d5e2c6ae6dcc60faa1f70c89b41013
Patrick Frank <patr.frank@gmx.de>
2020-03-28 18:03:04 +0100

    Add embedding partof triplet network

--- metric_learning/model_architecture.py
+++ metric_learning/model_architecture.py
@@ -8,12 +8,22 @@ class TripletLossLayer(keras.layers.Layer):
 		super(TripletLossLayer, self).__init__(**kwargs)
 
 	def triplet_loss(self, inputs):
-		anchor, positive, negative = inputs
+		anchor, positieve, negative = inputs
 		pos_dist = tf.reduce_sum(tf.square(anchor-positive), axis=-1)
 		neg_dist = tf.reduce_sum(tf.square(anchor-negative), axis=-1)
 		return tf.reduce_sum(tf.maximum(pos_dist - neg_dist + self.alpha, 0), axis=0)
 
 	def call(self, inputs):
-        loss = self.triplet_loss(inputs)
-        self.add_loss(loss)
-        return loss
+		loss = self.triplet_loss(inputs)
+		self.add_loss(loss)
+		return loss
+
+def embedding_model(input_shape, embedding_size):
+	'''Simple for now, only one dense relu layer'''
+	embedding = tf.keras.Sequential([
+		layers.Dense(embedding_size, activation='relu', input_shape=input_shape))
+	])
+	return embedding
+
+def triplet_network_model(input_shape, embedding_model, alpha=0.2):
+	
