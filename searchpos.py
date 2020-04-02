dcac7ae6e49fff0b8be9150318316dca641496d2
Patrick Frank <patr.frank@gmx.de>
2020-04-02 14:49:21 +0200

    Prototype convert_bb_to_board

--- searchpos.py
+++ searchpos.py
@@ -1,9 +1,11 @@
 import math
+
 import faiss
 import numpy as np
 import h5py
-from pgn2pos import correct_file_ending, board_to_bb
 import chess
+
+from pgn2pos import correct_file_ending, board_to_bb
 
 def init_binary_index(dim, threads=4):
 	# set threads
@@ -56,27 +58,63 @@ def search_bb(query_array, faiss_index, num_results=10):
 	D = faiss_index.search(query_array, k=num_results)
 	return D
 
+def convert_bb_to_board(bb):
+	rec_board = chess.Board()
+	rec_board.clear()
+	pieces = list(zip(6*["white"],["pawn","knight","bishop","rook","queen","king"]))
+	pieces = pieces + list(zip(6*["black"],["pawn","knight","bishop","rook","queen","king"]))
+	#print(pieces)
+	for color in [1, 0]:
+		for i in range(1, 7): # P N B R Q K / white
+			index = (1-color)*6+i-1
+			print(f"bitmask for pieces: {pieces[index]}")
+			print(f"start index: {index*64}")
+			print(f"end index: {(index+1)*64}")
+			bitmask = bb[index*64:(index+1)*64]
+			print(len(bitmask))
+			squares = np.argwhere(bitmask)
+			squares = [square for sublist in squares for square in sublist]
+			print(squares)
+			piece = chess.Piece(i,color)
+			for square in squares:
+				rec_board.set_piece_at(square,piece)
+	turn = bb[768]
+	castling_a1 = bb[769]
+	castling_h1 = bb[770]
+	castling_a8 = bb[771]
+	castling_h8 = bb[772]
+	print(turn,castling_a1,castling_a8,castling_h1,castling_h8)
+	print(bb[773:])
+	print(rec_board)
+
 if __name__ == "__main__":
 
 	# test loading
-	index = init_binary_index(776)
-	index = load_h5_array(["data/db/lichess_db_standard_rated_2013-01-bb"], "position_1", index)
-
-	# test querying
-	print("Testing.............")
-	board = chess.Board("rnb1kb1r/pp2pppp/2p2n2/3qN3/2pP4/6P1/PP2PP1P/RNBQKB1R w KQkq - 2 6")
+	print("\nTesting bitboard conversion")
+	fen = "rnb1kb1r/pp2pppp/2p2n2/3qN3/2pP4/6P1/PP2PP1P/RNBQKB1R w KQkq - 2 6"
+	board = chess.Board(fen)
 	print(board)
 	bitboard = board_to_bb(board)
 	print(bitboard.shape)
 	bitboard = np.concatenate((bitboard, np.zeros((3,),dtype=bool)))
 	print(bitboard.shape)
-	search_uint8 = bb_convert_bool_uint8(bitboard)
-	print(search_uint8)
-	dist, idx = search_bb(search_uint8, index)
-	print(dist, idx)
 
-	with h5py.File("data/db/lichess_db_standard_rated_2013-01-bb.h5", 'r') as hf:
-		for i in idx[0]:
-			near_board = hf["position_1"][i]
-			print(near_board)
-			# TODO: convert bitboard to chess.board
+	reconstructed_board = convert_bb_to_board(bitboard)
+	print(board)
+	
+
+	# test querying
+	# print("\nTesting index")
+	# index = init_binary_index(776)
+	# index = load_h5_array(["data/bitboards/lichess_db_standard_rated_2013-01-bb"], "position_1", index)
+	# # search board from previous test
+	# search_uint8 = bb_convert_bool_uint8(bitboard)
+	# print(search_uint8)
+	# dist, idx = search_bb(search_uint8, index)
+	# print(dist, idx)
+
+	# with h5py.File("data/bitboards/lichess_db_standard_rated_2013-01-bb.h5", 'r') as hf:
+	# 	for id in idx[0]:
+	# 		near_board = hf["position_1"][id]
+	# 		print(near_board)
+	# 		# TODO: convert bitboard to chess.board
