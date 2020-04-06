044e31b599323cd4ec12b4200c44b256aa79f810
Patrick Frank <patr.frank@gmx.de>
2020-04-06 22:30:33 +0200

    Create package structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name='chesspos',
+	version='0.1',
+	description='A library for searching and manipulating chess positions',
+	url='https://github.com/patrickfrank1/chess-pos/',
+	author='Patrick Frank',
+	author_email='[email protected]',
+	license='',
+	packages=['chesspos'],
+	zip_safe=False
+)
