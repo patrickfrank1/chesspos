4e687bc0343258a88e4eeff038d7a788fa53c0de
Patrick Frank <patr.frank@gmx.de>
2020-04-07 13:01:11 +0200

    Split pgn2pos in library functions and cli

--- chesspos/pgnextract.py
+++ chesspos/pgnextract.py
@@ -1,5 +1,3 @@
-import argparse
-
 import numpy as np
 from numpy.random import randint, shuffle
 import h5py
@@ -151,47 +149,3 @@ def save_tuples(tuples, file, dset_num=0):
 			dtype=bool, compression="gzip", compression_opts=9)
 
 		data1[:] = tuples[:]
-
-if __name__ == "__main__":
-
-	# https://stackoverflow.com/questions/29335145/python-argparse-extra-args/29335524#29335524
-	def ensure_value(namespace, dest, default):
-		stored = getattr(namespace, dest, None)
-		if stored is None:
-			return default
-		return stored
-
-	class store_dict(argparse.Action):
-		def __call__(self, parser, namespace, values, option_string=None):
-			vals = dict(ensure_value(namespace, self.dest, {}))
-			k, _, v = values.partition('=')
-			vals[k] = v
-			setattr(namespace, self.dest, vals)
-
-	parser = argparse.ArgumentParser(description='Generate bitboards and training samples')
-
-	parser.add_argument('input', type=str, action="store", help='pgn file with input games')
-	parser.add_argument('--save_position', type=str, action="store",\
-						help='h5py file to store the encoded positions')
-	parser.add_argument('--tuples', type=bool, default=False, action="store",\
-						help='h5py file to sore the encoded positions')
-	parser.add_argument('--save_tuples', type=str, action="store",\
-						help='h5py file to store the encoded tuples')
-	parser.add_argument('--chunksize', type=int, action="store", default=100000,\
-						help='Chunk size for paginating games')
-	parser.add_argument('--filter', default={}, action=store_dict,\
-						help="Filter out games. Options: time_min, elo_min. Usage: --filter key1=val1 --filter key2=val2",\
-						metavar="KEY1=VAL1")
-
-	args = parser.parse_args()
-
-	print(f"Input file at: {args.input}")
-	print(f"Filter options: {args.filter}")
-	print(f"Positions saved at: {args.save_position}")
-	print(f"Tuples generated: {args.tuples}")
-	print(f"Tuples saved at: {args.save_tuples}")
-	print(f"Chunksize: {args.chunksize}\n\n")
-
-	pgn_to_bitboard(args.input, save_file=args.save_position,
-					generate_tuples=args.tuples, tuple_file=args.save_tuples,
-					chunksize=args.chunksize, game_filter=args.filter)
