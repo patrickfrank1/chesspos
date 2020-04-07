6b436b0a0d84a8be5a97838c5d74b1283020d104
Patrick Frank <patr.frank@gmx.de>
2020-04-07 12:08:30 +0200

    Add files_from_directory to utils.py

--- chesspos/utils.py
+++ chesspos/utils.py
@@ -1,2 +1,7 @@
-def hello():
-	print("Hello, this is the utils package")
+from os import listdir
+from os.path import isfile, join
+
+def files_from_directory(directory):
+	file_arr = [f for f in listdir(directory) if isfile(join(directory, f))]
+	return file_arr
+
