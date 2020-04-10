6b1a9baea397944d0d05a61bbb827bfa8598d512
Patrick Frank <patr.frank@gmx.de>
2020-04-10 15:09:52 +0200

    After git rebase: fix train_model.py

--- tools/train_model.py
+++ tools/train_model.py
@@ -5,8 +5,9 @@ import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as plt
 
-from triplet_preparation import train_inputs_file_array_generator, train_inputs_length
-from model_architecture import triplet_network_model
+from chesspos.preprocessing import input_generator, input_length
+from chesspos.preprocessing import easy_triplets, hard_triplets
+from chesspos.models import triplet_autoencoder
 
 print(f"tf.__version__: {tf.__version__}") # pylint: disable=no-member
 print(f"tf.keras.__version__: {tf.keras.__version__}")
@@ -22,14 +23,13 @@ save_metrics = True
 hide_warnings = True
 plot_model = True
 # model specs
-input_shape = (773,)
-embedding_size = 10
+input_size = 773
+embedding_size = 32
 # training specs
 train_batch_size = 16
 validation_batch_size = 16
 train_steps_per_epoch = 1000
-validation_steps_per_epoch = 10
-yield_augmented = 1
+validation_steps_per_epoch = 50
 
 '''
 Training environment
@@ -73,7 +73,6 @@ class SkMetrics(keras.callbacks.Callback):
 		self.frac_correct.append(frac.numpy())
 		print(f" triplet_acc: {self.frac_correct[-1]}")
 
-
 '''
 Initialize triplet network
 '''
@@ -84,7 +83,6 @@ model = triplet_autoencoder(
 	triplet_weight_ratio=20.0,
 	hidden_layers=[512,0.2,256,0.2,64]
 )
-model = triplet_network_autoencoder(input_shape, embedding_size, hidden_layers=[512,256,64])
 
 if plot_model:
 	keras.utils.plot_model(model, model_dir+'/triplet_network.png', show_shapes=True)
@@ -93,8 +91,8 @@ if plot_model:
 Initialise trainig, and validation data
 '''
 train_files = [
-	#os.path.abspath('data/train_small/lichess_db_standard_rated_2013-02-tuples.h5'),
-	#os.path.abspath('data/train_large/lichess_db_standard_rated_2013-03-tuples.h5'),
+	os.path.abspath('data/train_small/lichess_db_standard_rated_2013-02-tuples.h5'),
+	os.path.abspath('data/train_large/lichess_db_standard_rated_2013-03-tuples.h5'),
 	os.path.abspath('data/train_large/lichess_db_standard_rated_2013-04-tuples.h5')
 ]
 validation_files = [
@@ -102,23 +100,39 @@ validation_files = [
 ]
 
 # TODO: print WARNING if too few validation examples
-train_len = pp.input_length(train_files, table_id_prefix="tuples")
-val_len = pp.input_length(validation_files, table_id_prefix="tuples")
+train_len = input_length(train_files, table_id_prefix="tuples")
+val_len = input_length(validation_files, table_id_prefix="tuples")
 print(f"\n{train_len} training samples.")
 print(f"{val_len} validation samples.")
 
+# sampling functions
+train_fn = [easy_triplets, hard_triplets]
+validation_fn = [hard_triplets]
+
 # generators for train and test data
-train_generator = train_inputs_file_array_generator(train_files, table_id_prefix="tuples",
-					tuple_indices=[0,1,2,3,4,5,6], batch_size=train_batch_size)
-validation_generator = train_inputs_file_array_generator(validation_files, table_id_prefix="tuples",
-					tuple_indices=[0,1,2,3,4,5,6], batch_size=validation_batch_size)
-metric_generator = train_inputs_file_array_generator(validation_files, table_id_prefix="tuples",
-					tuple_indices=[0,1,2,3,4,5,6], batch_size=validation_batch_size)
+train_generator = input_generator(
+	train_files,
+	table_id_prefix="tuples",
+	selector_fn=train_fn,
+	batch_size=train_batch_size
+)
+validation_generator = input_generator(
+	validation_files,
+	table_id_prefix="tuples",
+	selector_fn=validation_fn,
+	batch_size=validation_batch_size
+)
+metric_generator = input_generator(
+	validation_files,
+	table_id_prefix="tuples",
+	selector_fn=validation_fn,
+	batch_size=validation_batch_size
+)
 
 # instantiate callbacks
 skmetrics = SkMetrics(metric_generator, batch_size=validation_batch_size, steps_per_callback=10)
 early_stopping = keras.callbacks.EarlyStopping(
-	monitor='val_loss', min_delta=0.1, patience=10, verbose=0, mode='min'
+	monitor='val_loss', min_delta=0.05, patience=10, verbose=0, mode='min'
 )
 cp = keras.callbacks.ModelCheckpoint(
 	filepath=model_dir+"/checkpoints/cp-{epoch:04d}.ckpt",
@@ -132,7 +146,7 @@ cp = keras.callbacks.ModelCheckpoint(
 history = model.fit(
 	train_generator,
 	steps_per_epoch=train_steps_per_epoch,
-	epochs=int(yield_augmented*train_len/train_steps_per_epoch/train_batch_size),
+	epochs=int(len(train_fn)*train_len/train_steps_per_epoch/train_batch_size),
 	validation_data=validation_generator,
 	validation_steps=validation_steps_per_epoch,
 	callbacks=[skmetrics, early_stopping] #, cp]
