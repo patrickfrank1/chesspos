36b26c9005967fbffe2e51e5f66caaa0cda19b55
Patrick Frank <patr.frank@gmx.de>
2020-04-19 14:33:21 +0200

    Package index_from_embedding as cli

--- tools/index_from_embedding.py
+++ tools/index_from_embedding.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 
 import h5py
@@ -6,44 +7,86 @@ import faiss
 from chesspos.utils import files_from_directory, correct_file_ending
 import chesspos.embedding_index as iemb
 
-# inputs
-index_factory_string = "PCA16,SQ4"
-embedding_dir = "/media/pafrank/Backup/other/Chess/lichess/embeddings/bb_d64_add"
-save_path = "/media/pafrank/Backup/other/Chess/lichess/embeddings"
-table_prefix = "test_embedding"
-queries = [
-	"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-	"8/1R6/4p1k1/1p6/p1b2K2/P1Br4/1P6/8 b - - 8 49"
-]
-num_results = 10
-embedding_dim = None
-train_frac = 1e-3
-chunks = int(1e4)
+def index_from_embedding(index_factory_string, embedding_dir, table_prefix="test_embedding",
+	save_path="", chunks=int(1e4), train_frac=1e-3):
 
-# get list of embedding files
-embedding_list = files_from_directory(embedding_dir, file_type="h5")
+	# get list of embedding files
+	embedding_list = files_from_directory(embedding_dir, file_type="h5")
 
-# infer embedding dimension from provided embeddings
-fname = correct_file_ending(embedding_list[0], "h5")
-with h5py.File(fname, 'r') as hf:
-	for key in hf.keys():
-		if table_prefix in key:
-			embedding_dim = len(hf[key][0])
-			break
+	# infer embedding dimension from provided embeddings
+	embedding_dim = None
+	fname = correct_file_ending(embedding_list[0], "h5")
+	with h5py.File(fname, 'r') as hf:
+		for key in hf.keys():
+			if table_prefix in key:
+				embedding_dim = len(hf[key][0])
+				break
 
-# create index
-index = faiss.index_factory(embedding_dim, index_factory_string)
+	# create index
+	index = faiss.index_factory(embedding_dim, index_factory_string)
 
-# train faiss index
-index = iemb.index_train_embeddings(embedding_list, table_prefix, index,
-	train_frac=train_frac, chunks=chunks
-)
+	# train faiss index
+	index = iemb.index_train_embeddings(embedding_list, table_prefix, index,
+		train_frac=train_frac, chunks=chunks
+	)
 
-# populate faiss index
-index, table_dict = iemb.index_load_file_array(embedding_list, table_prefix,
-	index, chunks=chunks
-)
+	# populate faiss index
+	index, table_dict = iemb.index_load_file_array(embedding_list, table_prefix,
+		index, chunks=chunks
+	)
 
-# save index
-faiss.write_index(index, f"{save_path}/{index_factory_string}.faiss")
-json.dump( table_dict, open( f"{save_path}/{index_factory_string}.json", 'w' ) )
+	# save index
+	faiss.write_index(index, f"{save_path}/{index_factory_string}.faiss")
+	json.dump( table_dict, open( f"{save_path}/{index_factory_string}.json", 'w' ) )
+
+	return "Done."
+
+
+if __name__ == "__main__":
+
+	parser = argparse.ArgumentParser(description='Generate a searchable faiss index from float embeddings.')
+
+	parser.add_argument('index_factory_string', type=str, action="store",
+		help='use a valid string to initialize the faiss index factory'
+	)
+	parser.add_argument('embedding_dir', type=str, action="store",
+		help='path to directory that contains all embeddings in h5 files, which should be added to the index'
+	)
+	parser.add_argument('--table_prefix', type=str, default="test_embedding",
+	help='the prefix that selects all h5 tables that store embeddings (default="test_embedding")'
+	)
+	parser.add_argument('--save_path', type=str, action='store', default="",
+		help="full path- and filename to the output index, defaults to the input directory"
+	)
+	parser.add_argument('--chunks', type=int, default=int(1e4),
+		help='chunksize in which files are read, decrease if you run out of RAM'
+	)
+	parser.add_argument('--train_frac', type=float, default=1e-3,
+		help='number of train samples as fraction of the total number of samples (default=1e-3)'
+	)
+
+	args = parser.parse_args()
+
+	# print inputs for the user
+	print(f"Index factory string: {args.index_factory_string}")
+	print(f"Input directory: {args.embedding_dir}")
+	print(f"Table key for h5 tables: {args.table_prefix}")
+	print(f"Save path: {args.save_path}")
+	print(f"Chunks: {args.chunks}")
+	print(f"Training fraction: {args.train_frac}\n\n")
+
+	save = ""
+	if args.save_path =="":
+		save = args.embedding_dir
+	else:
+		save = args.save_dir
+
+	out = index_from_embedding(
+		args.index_factory_string,
+		args.table_prefix,
+		args.embedding_dir,
+		save_path=save,
+		chunks=args.chunks,
+		train_frac=args.train_frac
+	)
+	print(out)
